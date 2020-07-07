93621b465cdbb1b59ab7a35e8db8621db8cc11ed
Mark Liang <mliang@mozilla.com>
2020-07-07 16:35:44 +0800

    Updates

---
+++ precache-manifest.dde990d4b5ec1e3c02404c54dec115d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19c0be93337888aee22ca8315400d839",
+    "url": "/play/index.html"
+  },
+  {
+    "revision": "529a814f95fecf44b8e9",
+    "url": "/play/static/css/main.e1c278dd.chunk.css"
+  },
+  {
+    "revision": "8542d867923306e87fb5",
+    "url": "/play/static/js/2.f4453c39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/play/static/js/2.f4453c39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "529a814f95fecf44b8e9",
+    "url": "/play/static/js/main.5eb604ec.chunk.js"
+  },
+  {
+    "revision": "2836e9e0bf51247e1ce3",
+    "url": "/play/static/js/runtime-main.93026873.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/play/static/media/logo.5d5d9eef.svg"
+  }
+]);
